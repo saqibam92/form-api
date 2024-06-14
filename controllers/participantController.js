af80f5addf1c62c8f8ad4f2b0a92f47cba408b8d
saqibam92 <saqibam0@gmail.com>
2024-06-14 21:32:16 +0600

    update paticipant controller

--- controllers/participantController.js
+++ controllers/participantController.js
@@ -6,6 +6,10 @@ const createParticipant = async (req, res) => {
     await participant.save();
     res.status(201).json(participant);
   } catch (error) {
+    if (error.code === 11000) {
+      // Duplicate key error code
+      return res.status(400).json({ message: "Mobile Number Already in use" });
+    }
     res.status(400).json({ message: error.message });
   }
 };
